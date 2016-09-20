13f07ea1b3d99f7326eaa321c2477b12059dd2f8
Geoff Williams <geoff.williams@puppetlabs.com>
2016-09-20 23:22:55 +1000

    GET works

--- lib/pe_rbac.rb
+++ lib/pe_rbac.rb
@@ -1,36 +1,41 @@
 require "pe_rbac/version"
 require 'restclient'
-require 'yaml'
+require 'socket'
 
 module PeRbac
-  CONFIGFILE = "#{Puppet.settings[:confdir]}/classifier.yaml"
+  ssldir = '/etc/puppetlabs/puppet/ssl'
+  CONF = {
+    host: Socket.gethostname,
+    port: 4433,
+    cert: ssldir + '/certs/pe-internal-orchestrator.pem',
+    key: ssldir + '/private_keys/pe-internal-orchestrator.pem',
+    cacert: ssldir + '/certs/ca.pem'
+  }
 
-  # This is autoloaded by the master, so rescue the permission exception.
-  CONF = YAML.load_file(CONFIGFILE)
   BASE_URI = '/rbac-api/v1'
 
   #
   # user
   # 
 
-  def get_users
+  def self.get_users
     _request(:get, '/users')
   end
 
-  def get_user(id)
+  def self.get_user(id)
     _request(:get, "/users/#{id}")
   end
 
-  def create_user
+  def self.create_user(display_name, login, email)
     user = {
       "userName"        => nil,
-      "displayName"     => "aaa",
-      "login"           => "bbbb",
-      "email"           => "",
+      "displayName"     => display_name,
+      "login"           => login,
+      "email"           => email,
       "isSuperuser"     => false,
       "isRemote"        => false,
       "isRevoked"       => false,
-      "lastLogin"       => null,
+      "lastLogin"       => nil,
       "roles"           => [],
       "groups"          => [],
       "inheritedRoles"  => [],
@@ -38,24 +43,23 @@ module PeRbac
     _request(:post, '/users', user)
   end
 
-  def update_user(id, login, email, display_name, roles=[], remote=false, superuser=false,
+  def self.update_user(id, login, email, display_name, roles=[], remote=false, superuser=false)
     user = {
-      'is_revoked'   => revoked?,
+      'is_revoked'   => false,
       'id'           => id,
       'login'        => login,
       'email'        => email,
       'display_name' => display_name,
-      'role_ids'     => roles),
+      'role_ids'     => roles,
       'is_remote'    => remote,
       'is_superuser' => superuser,
       #'last_login'   => @property_hash[:last_login],  << WTF puppet... stop smoking bongs
       'is_group'     => false,
     }
-
     _request(:put, "/users/#{user['id']}", user)
   end
 
-  def revoke_user(id, revoke=true)
+  def self.revoke_user(id, revoke=true)
     user = {
       'is_revoked' => revoke,
       'id'         => id,
@@ -66,23 +70,23 @@ module PeRbac
   #
   # role
   #
-  def get_roles
+  def self.get_roles
     _request(:get, "/roles")
   end
 
-  def get_role(id)
+  def self.get_role(id)
     _request(:get, "/roles/#{id}")
   end
 
   # doesn't seem possible to DELETE roles!
-  def role(display_name, role_name, description, permissions=[], users=[])
+  def self.role(display_name, role_name, description, permissions=[], users=[])
     
     role = {
       "displayName" => display_name,
       "roleName"    => role_name,
       "description" => description,
       "permissions" => permissions,
-      "users":      => users,
+      "users"       => users,
       "groups"      => [], # doesn't seem to be used yet
     }
 
@@ -91,14 +95,14 @@ module PeRbac
 
   private
 
-  def _request(method, path, payload = nil)
-    url = "https://#{CONF['server']}:#{CONF['port']}#{BASE_URI}#{path}"
+  def self._request(method, path, payload = nil)
+    url = "https://#{CONF[:host]}:#{CONF[:port]}#{BASE_URI}#{path}"
     RestClient::Request.execute(
       method: method, 
       url: url,
-      ssl_ca_file: Puppet.settings[:localcacert],
-      ssl_client_cert: OpenSSL::X509::Certificate.new(File.read(Puppet.settings[:hostcert])),
-      ssl_client_key: OpenSSL::PKey::RSA.new(File.read(Puppet.settings[:hostprivkey])),
+      ssl_ca_file: CONF[:cacert],
+      ssl_client_cert: OpenSSL::X509::Certificate.new(File.read(CONF[:cert])),
+      ssl_client_key: OpenSSL::PKey::RSA.new(File.read(CONF[:key])),
       ssl_version: :TLSv1,
       payload: payload,
     )
