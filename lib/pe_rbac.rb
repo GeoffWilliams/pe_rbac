a23247c31bedb9bf0f11e456889b4963f6285b0b
Geoff Williams <geoff.williams@puppetlabs.com>
2016-09-21 12:57:44 +1000

    json everywhere, install tokes

--- lib/pe_rbac.rb
+++ lib/pe_rbac.rb
@@ -20,31 +20,40 @@ module PeRbac
   # 
 
   def self.get_users
-    _request(:get, '/users')
+    JSON.parse(_request(:get, '/users').body)
   end
 
-  # get the user id for a login
+  # get the user id for a login or false if missing
   # eg 'admin' => '42bf351c-f9ec-40af-84ad-e976fec7f4bd'
   def self.get_user_id(login)
-    users = JSON.parse(get_users.body)
-    id = nil
+    users = get_users
+    id = false
     i = 0
-    found = false
-    while !found and i < users.length do
+    while !id and i < users.length do
       if users[i]['login'] == login
         id = users[i]['id']
-        found = true
       end
       i += 1
-    end
-    if ! found
-      raise("Login #{login} does not exist")
     end
     id
   end
 
   def self.get_user(id)
-    _request(:get, "/users/#{id}")
+    JSON.parse(_request(:get, "/users/#{id}").body)
+  end
+
+  def self.ensure_user(login, email, display_name, password=nil, role_ids=[])
+    if get_user_id(login)
+      # existing user
+      update_user(login, email, display_name, role_ids)
+      if password
+        change_password(login, password)
+      end
+    else
+      # new user
+      create_user(login, email, display_name, password, role_ids)
+    end
+
   end
 
   def self.create_user(login, email, display_name, password=nil, role_ids=[])
@@ -64,7 +73,7 @@ module PeRbac
   end
 
   def self.update_user(login, email=nil, display_name=nil, role_ids=nil, is_revoked=nil)
-    user = JSON.parse(get_user(get_user_id(login)).body)
+    user = get_user(get_user_id(login))
     if ! user['remote']
       # trade-off for auto id lookup is that you cant change logins...
       user['login'] = login ? login : user['login']
@@ -92,72 +101,114 @@ module PeRbac
   # role
   #
   def self.get_roles
-    _request(:get, "/roles")
+    JSON.parse(_request(:get, "/roles").body)
   end
 
   def self.get_role(id)
-    _request(:get, "/roles/#{id}")
+    JSON.parse(_request(:get, "/roles/#{id}").body)
   end
 
+  def self.get_role_id(display_name)
+    i=0
+    found=false
+    roles = get_roles()
+    while !found and i < roles.length do
+      if roles[i]['display_name'] == display_name
+        found = roles[i]['id']
+      end
+      i+=1
+    end
+    found
+  end
+
+  
   # get the role id for a display name
-  # eg 'Code Deployers' => 4
+  # eg ['Code Deployers', 'blah'] => [4,8]
   def self.get_role_ids(display_names)
     if ! display_names.is_a? Array
       display_names = [display_names]
     end
-    roles = JSON.parse(get_roles.body)
+    roles = get_roles
     ids = []
     display_names.each { |display_name|
-      i=0
-      found=false
-      while !found and i < roles.size do
-        if roles[i]['display_name'] == display_name
-          ids.push(roles[i]['id'])
-          found=true
-        end
-        i+=1
-      end
+      found=get_role_id(display_name)
       if !found
         raise("RBAC role '#{display_name}' not found")
       end
+      ids.push(found)
     }
     ids
   end
 
-  # doesn't seem possible to DELETE roles!
-  def self.role(display_name, role_name, description, permissions=[], users=[])
-    
-    role = {
-      "displayName" => display_name,
-      "roleName"    => role_name,
-      "description" => description,
-      "permissions" => permissions,
-      "users"       => users,
-      "groups"      => [], # doesn't seem to be used yet
-    }
-
-    _request(:post, "/roles", role)
+  # CREATE
+  def self.ensure_role(display_name, description, permissions=[], users=[])
+    if get_role_id(display_name)
+      update_role(display_name, description, permissions, users)
+    else
+      create_role(display_name, description, permissions, users)
+    end
   end
+  
+  def self.create_role(display_name, description, permissions=[], users=[])
+    role = {
+      "display_name"  => display_name,
+      "description"   => description,
+      "permissions"   => permissions,
+      "user_ids"      => users,
+      "group_ids"     => [], # doesn't seem to be used yet
+    }
+    _request(:post, '/roles', role)
+  end
+
+  def self.update_role(display_name, description=nil, permissions=nil, user_ids=nil, group_ids=nil)
+    role_id = get_role_id(display_name)
+    if role_id 
+      role = get_role(role_id)
+      role['display_name'] = display_name ? display_name : role['display_name']
+      role['description'] = description ? display_name : role['description']
+      role['permissions'] = permissions ? display_name : role['permissions']
+      role['user_ids'] = user_ids ? display_name : role['user_ids']
+      role['group_ids'] = group_ids ? display_name : role['group_ids']
+
+      _request(:put, "/roles/#{role_id}", role)
+    else
+      raise("No such role exists: #{display_name} create it first or use ensure_role")
+    end
+  end
+  
+
+  #
+  # Permissions
+  #
+  def self.get_permissions()
+    _request(:get, "/types")
+  end
+
 
   #
   # Token
   #
-  def self.token(login, password)
+  def self.token(login, password, lifetime=false)
     payload = {
       "login"     => login,
       "password"  => password,
     }
+    
+    # see https://docs.puppet.com/pe/latest/rbac_token_auth.html#setting-a-token-specific-lifetime
+    if lifetime
+      payload["lifetime"] = lifetime
+    end
 
     JSON.parse(_request(:post, '/auth/token', payload))['token']
   end
 
-  def self.login(login, password)
+  def self.login(login, password, lifetime=false)
     dirname = Dir.home + '/.puppetlabs'
     tokenfile = dirname + '/token'
     if ! Dir.exist?(dirname)
       Dir.mkdir(dirname, 0700)
     end
-    File.write(tokenfile, token(login, password))
+    File.write(tokenfile, token(login, password, lifetime))
     File.chmod(0600, tokenfile)
   end
 
