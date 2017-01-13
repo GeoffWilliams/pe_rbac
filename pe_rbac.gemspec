9c66caabb2bbea5f2e501f1ca769041b1be72493
Geoff Williams <geoff.williams@puppetlabs.com>
2017-01-14 00:04:14 +1100

    fakefs support for testing writing out tokens

--- pe_rbac.gemspec
+++ pe_rbac.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "sinatra"
   spec.add_development_dependency "webrick"
   spec.add_development_dependency "rack"
+  spec.add_development_dependency "fakefs"
   spec.add_development_dependency "rspec", "~> 3.0"
 
 
--- spec/pe_rbac/action_spec.rb
+++ spec/pe_rbac/action_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
 require 'pe_rbac/action'
+require 'fakefs/safe'
 describe PeRbac::Action do
   it "changes the password successfully" do
 
@@ -16,6 +17,23 @@ describe PeRbac::Action do
     expect(token).to eq false
   end
 
+  it "creates token file for logged in user" do
+
+    # use a fakefs to avoid overwriting real files on the system, we have to
+    # also clone our fake SSL certs as well
+    config = File.expand_path('./spec')
+    FakeFS::FileSystem.clone(config, '/spec')
+    FakeFS do
+      # create a fake homedir on our fakefs to hold the fake token :)
+      FileUtils.mkdir_p Dir.home
+
+      PeRbac::Action::login('admin', 'password')
+
+      token = File.read(File.join(Dir.home, '/.puppetlabs', '/token'))
+      expect(token).to eq 'DEADBEEF'
+    end
+
+  end
   # token
   # login
   # reset_password
